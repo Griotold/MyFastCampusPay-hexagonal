fe885f14bf471198c8713798c91256a4c7cf5a8a
Griotold <gotjd9773@naver.com>
2024-06-28 17:11:04 +0900

    test controller

---
+++ src/main/java/org/study/myfastcampuspay/MyFastCampusPayApplication.java
@@ -0,0 +1,13 @@
+package org.study.myfastcampuspay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFastCampusPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFastCampusPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/study/myfastcampuspay/MyFastCampusPayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.study.myfastcampuspay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFastCampusPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
